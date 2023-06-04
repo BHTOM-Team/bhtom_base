1578e14cb0026d42c06654666d462c7fd1e0a427
patrik <patrik.sivak.314@gmail.com>
2023-06-04 04:52:22 +0200

    Url slug

--- bhtom_targets/views.py
+++ bhtom_targets/views.py
@@ -400,7 +400,8 @@ class TargetDetailView(Raise403PermissionRequiredMixin, DetailView):
     """
     permission_required = 'bhtom_targets.view_target'
     model = Target
-
+    slug_field = 'name'
+    slug_url_kwarg = 'name'
     def get_context_data(self, *args, **kwargs):
         """
         Adds the ``DataProductUploadForm`` to the context and prepopulates the hidden fields.
